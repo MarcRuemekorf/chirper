72b215e252d7f2e005567d4c473e0459bc8cbeb4
Marc Ruemekorf <mruemekorf@enrise.com>
2024-01-19 16:41:36 +0100

    Add chirp logic and chirp component

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import Chirp from '@/Components/Chirp';
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { useForm, Head } from '@inertiajs/react';
 
-export default function Index({auth}) {
+export default function Index({auth, chirps}) {
     const {data, setData, post, processing, reset, errors} = useForm({
         message: '',
     });
@@ -17,7 +18,7 @@ export default function Index({auth}) {
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Chirps" />
-            <div className="max-2-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
                     <textarea
                         value={data.message}
@@ -28,6 +29,11 @@ export default function Index({auth}) {
                         <InputError message={errors.message} className="mt-2" />
                         <PrimaryButton className="mt-4" disabled={processing}>Chirp</PrimaryButton>
                 </form>
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {chirps.map(chirp =>
+                        <Chirp key={chirp.id} chirp={chirp} />
+                    )}
+                </div>
             </div>
         </AuthenticatedLayout>
     )
